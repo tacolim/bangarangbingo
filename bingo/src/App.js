0ac08556eb33fd5f42c59d0f6575748e428b5975
Cassidy SN Avery <tacolim@gmail.com>
2018-03-22 16:06:03 -0400

    react app created & functioning as expected

---
+++ bingo/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      // eslint-disable-next-line react/jsx-filename-extension
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Bangarang Bingo</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
