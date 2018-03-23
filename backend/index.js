d00c4a0310f5bc15c45081dba54597a3e7357104
Christian Franco <christian.franco94@icloud.com>
2018-03-23 13:19:48 -0700

    some routing

--- backend/index.js
+++ backend/index.js
@@ -6,4 +6,16 @@ server.get('/cards', (req, res) => {
   res.json([]);
 });
 
+server.get('/card/:id', (req, res) => {
+  res.json({});
+});
+
+server.post('/card/create', (req, res) => {
+  res.json({});
+});
+
+server.post('/card/edit', (req, res) => {
+  res.json({});
+});
+
 server.listen(3000);
