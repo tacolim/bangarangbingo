378dcb354b3f69ab5b8169462dcd04caa8cf34a8
Cassidy SN Avery <tacolim@gmail.com>
2018-03-27 14:34:05 -0400

    css to organize elements on landing page

--- bingo/src/components/landing.js
+++ bingo/src/components/landing.js
@@ -3,20 +3,31 @@ import { Link } from 'react-router-dom';
 import './landing.css';
 
 const Landing = () => (
-  <div>
-    <div>
-      <div><Link to="/SignUp">Sign Up</Link></div>
-      <div><Link to="/SignIn">Sign In</Link></div>
+  <div className="landing">
+    <div className="navlinks">
+      <div><Link to="/SignUp" className="navlinks__button">Sign Up</Link></div>
+      <div><Link to="/SignIn" className="navlinks__button">Sign In</Link></div>
     </div>
-    <h1>Bangarang Bingo</h1>
-    <div className="icon-cards">
-      <div className="icon-cards__content">
-        <div className="icon-cards__item">BINGO</div>
-        <div className="icon-cards__item">BINGO</div>
-        <div className="icon-cards__item">BINGO</div>
+    <div className="landing__content">
+      <div className="header">Bangarang Bingo</div>
+      <div className="carousel">
+        <div className="icon-cards">
+          <div className="icon-cards__content">
+            <div className="icon-cards__item">BINGO</div>
+            <div className="icon-cards__item">BINGO</div>
+            <div className="icon-cards__item">BINGO</div>
+          </div>
+        </div>
       </div>
+      <div className="text">
+        <p>Making your own bingo cards is time consuming and difficult.</p>
+        <p>Finally, an easy way to make beautiful,
+          print ready bingo cards for your parties and events.
+        </p>
+        <p>Simply input your words, choose some styling options, and we do the rest!</p>
+      </div>
+      <div><Link to="/SignUp" className="links__button">Make Your Bingo Card Now!</Link></div>
     </div>
-    <div><Link to="/SignUp">Make Your Bingo Card Now!</Link></div>
   </div>
 );
 
